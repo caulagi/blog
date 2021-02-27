204edeea2c3cb17c5c68195827bd3720e5e059f9
Pradip Caulagi <caulagi@gmail.com>
2021-02-27 19:52:15 +0100

    Make avatar fixed to show my image
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,4 +1,5 @@
 import { CMS_NAME } from '../lib/constants'
+import Avatar from './avatar'
 
 const Intro = () => {
   return (
@@ -7,14 +8,7 @@ const Intro = () => {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+        <Avatar />
       </h4>
     </section>
   )
