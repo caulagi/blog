f632c9699db1d103b97b93023562b2a33b687fab
Pradip Caulagi <caulagi@gmail.com>
2021-02-27 18:49:14 +0100

    Remove alert component (#3)
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -15,16 +15,15 @@ import PostType from '../../types/post'
 type Props = {
   post: PostType
   morePosts: PostType[]
-  preview?: boolean
 }
 
-const Post = ({ post, morePosts, preview }: Props) => {
+const Post = ({ post, morePosts }: Props) => {
   const router = useRouter()
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
   }
   return (
-    <Layout preview={preview}>
+    <Layout>
       <Container>
         <Header />
         {router.isFallback ? (
