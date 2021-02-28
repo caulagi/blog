8e64c70bcb4b2d218705f0410793204cdf7f22a7
Pradip Caulagi <caulagi@gmail.com>
2021-02-28 11:05:32 +0100

    Show published date in color
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -6,7 +6,7 @@ interface DateFormatterProps {
 
 const DateFormatter: React.FC<DateFormatterProps> = ({ dateString }) => {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <time dateTime={dateString} className="italic text-purple-500">{format(date, 'LLLL	d, yyyy')}</time>
 }
 
 export default DateFormatter
