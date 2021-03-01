8e42f8545958f07f1fe9e4136445856ccbf5a8d0
Pradip Caulagi <caulagi@gmail.com>
2021-03-01 20:42:48 +0100

    Add google analytics
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
