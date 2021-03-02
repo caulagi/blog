f52789f28c439ab97e472745fc2265b7138f4b20
Pradip Caulagi <caulagi@gmail.com>
2021-03-02 17:01:36 +0100

    Always show avatar and date below title (#31)
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -3,7 +3,7 @@ const Avatar: React.FC = () => {
     <div className="flex -space-x-1 overflow-hidden">
       <img
         src="/assets/blog/authors/pradip.jpg"
-        className="h-32 w-32 rounded-full"
+        className="h-32 w-32 lg:h-38 lg:w-38 xl:h-48 xl:w-48 rounded-full"
         alt="{AUTHOR_NAME} photo"
       />
     </div>
