2aabe59edc6dcf22270bac936a4c73fadd36b452
Pradip Caulagi <caulagi@gmail.com>
2021-03-03 20:23:03 +0100

    Fix alt text for avatar
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,10 +1,12 @@
+import { AUTHOR_NAME } from '../lib/constants'
+
 const Avatar: React.FC = () => {
   return (
     <div className="flex -space-x-1 overflow-hidden">
       <img
         src="/assets/blog/authors/pradip.jpg"
         className="h-32 w-32 lg:h-38 lg:w-38 xl:h-48 xl:w-48 rounded-full"
-        alt="{AUTHOR_NAME} photo"
+        alt={`Photo of ${AUTHOR_NAME}`}
       />
     </div>
   )
