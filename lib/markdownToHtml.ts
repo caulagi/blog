2b531adfc84dcfe6b45b6d34e89d81c4de6d3e4d
Pradip Caulagi <caulagi@gmail.com>
2021-09-17 23:18:26 +0200

    Update remark to 14.0.1
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,9 +1,13 @@
-import remark from 'remark'
-import html from 'remark-html'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkHtml from 'remark-html'
 
 export default async function markdownToHtml(
   markdown: string
 ): Promise<string> {
-  const result = await remark().use(html).process(markdown)
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(markdown)
   return result.toString()
 }
