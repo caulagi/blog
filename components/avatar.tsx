ac87b8ac48aeb15f3cf90c84a780ef675e5222e2
Pradip Caulagi <caulagi@gmail.com>
2023-08-28 20:56:01 +0200

    Make avatar a bit larger (#511)

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -2,8 +2,11 @@ import { Avatar as NextUiAvatar } from '@nextui-org/react'
 
 const Avatar: React.FC = () => {
   return (
-    <div className="flex -space-x-1 overflow-hidden">
-      <NextUiAvatar src="/assets/blog/authors/pradip.jpg" />
+    <div className="flex gap-4 items-center">
+      <NextUiAvatar
+        src="/assets/blog/authors/pradip.jpg"
+        className="w-32 h-32"
+      />
     </div>
   )
 }
