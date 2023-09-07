64156d17388d930824d278e485a802cf12ee006b
Pradip Caulagi <caulagi@gmail.com>
2023-09-07 20:55:23 +0200

    fix build

--- components/quotation.tsx
+++ components/quotation.tsx
@@ -1,5 +1,3 @@
-import { Card, CardBody, CardHeader, Divider } from '@nextui-org/react'
-
 interface QuotationProps {
   author: string
   quotation: string
@@ -12,19 +10,19 @@ const Quotation: React.FC<QuotationProps> = ({
   reference,
 }) => {
   return (
-    <div class="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-      <div class="p-6">
+    <div className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="p-6">
         {reference && (
           <div>
-            <h5 class="mb-2 block font-sans font-semibold leading-snug">
+            <h5 className="mb-2 block font-sans font-semibold leading-snug">
               {reference}
             </h5>
-            <hr class="border-gray-300 m-1 lg:m-1" />
+            <hr className="border-gray-300 m-1 lg:m-1" />
           </div>
         )}
-        <div class="flex float-right">{author}</div>
-        <div class="clear-both"></div>
-        <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+        <div className="flex float-right">{author}</div>
+        <div className="clear-both"></div>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
           {quotation}
         </p>
       </div>
