aa6b132f7aa1a5408dd605488d624d21343c2f2b
Pradip Caulagi <caulagi@gmail.com>
2023-10-19 11:26:30 +0200

    chore: update remark to new major version (15.0.1)

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,15 +1,11 @@
 import { remark } from 'remark'
-import remarkDirective from 'remark-directive'
 import remarkHtml from 'remark-html'
-import remarkParse from 'remark-parse'
 import remarkPrism from 'remark-prism'
 
 export default async function markdownToHtml(
   markdown: string,
 ): Promise<string> {
   const r = remark()
-  r.use(remarkParse)
-  r.use(remarkDirective)
   r.use(remarkPrism)
   r.use(remarkHtml, { sanitize: false })
   return (await r.process(markdown)).toString()
