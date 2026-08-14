014f841537645195925cc3068dea9551d977b860
Pradip Caulagi <caulagi@gmail.com>
2026-08-14 08:42:04 +0200

    Add the remaining profile links to the about page structured data

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,8 +6,17 @@ export const SITE_DESCRIPTION =
   'Notes on distributed systems, platform engineering and the tools in between.'
 export const MASTODON_URL = 'https://mastodon.social/@caulagi'
 export const GITHUB_URL = 'https://github.com/caulagi'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/pradipcaulagi/'
+export const X_URL = 'https://x.com/caulagi'
+export const PERSONAL_SITE_URL = 'https://caulagi.com/'
 // Profiles claimed as the same person in the /about JSON-LD.
-export const SAME_AS = [GITHUB_URL, MASTODON_URL]
+export const SAME_AS = [
+  PERSONAL_SITE_URL,
+  GITHUB_URL,
+  MASTODON_URL,
+  LINKEDIN_URL,
+  X_URL,
+]
 export const EMAIL = 'caulagi@gmail.com'
 export const HOME_OG_IMAGE_URL =
   'https://og-image.now.sh/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg'
